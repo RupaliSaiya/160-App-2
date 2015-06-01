89c4a9421c5fc3df6ecc08b6807a0ebb0d754022
ubo32 <ubojorquez@ucdavis.edu>
2015-06-01 14:20:53 -0700

    adding app

---
+++ FriendShips/app/src/main/java/com/rau/friendships/Register.java
@@ -0,0 +1,58 @@
+package com.rau.friendships;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class Register extends Activity {
+
+    DatabaseHelper helper = new DatabaseHelper(this);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+
+    public void onSignUpClick(View v)
+    {
+        if(v.getId()== R.id.Bsignupbutton)
+        {
+            EditText name = (EditText)findViewById(R.id.TFname);
+            EditText email = (EditText)findViewById(R.id.TFemail);
+            EditText uname = (EditText)findViewById(R.id.TFuname);
+            EditText pass1 = (EditText)findViewById(R.id.TFpass1);
+            EditText pass2 = (EditText)findViewById(R.id.TFpass2);
+
+            String namestr = name.getText().toString();
+            String emailstr = email.getText().toString();
+            String unamestr = uname.getText().toString();
+            String pass1str = pass1.getText().toString();
+            String pass2str = pass2.getText().toString();
+
+            if(!pass1str.equals(pass2str)){
+
+                // show error message
+                String error = "Passwords do NOT match.";
+                Toast pass = Toast.makeText(Register.this, error, Toast.LENGTH_SHORT);
+                pass.show();
+            }
+            else{
+
+                // insert into database
+                User c = new User();
+                c.setName(namestr);
+                c.setEmail(emailstr);
+                c.setUname(unamestr);
+                c.setPass(pass1str);
+                helper.insertContact(c);
+
+                // direct to welcome screen
+                Intent i = new Intent(Register.this, Welcome.class);
+                startActivity(i);
+            }
+        }
+    }
+}
