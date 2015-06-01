470f43e0e3ba068d14b0d90c01e4e7bd53336ccd
ubo32 <ubojorquez@ucdavis.edu>
2015-06-01 15:26:14 -0700

    updates

--- FriendShips/app/src/main/java/com/rau/friendships/Welcome.java
+++ FriendShips/app/src/main/java/com/rau/friendships/Welcome.java
@@ -4,6 +4,7 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 
 public class Welcome extends ActionBarActivity {
@@ -12,6 +13,9 @@ public class Welcome extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+        String username = getIntent().getStringExtra("Username");
+        TextView tv = (TextView)findViewById(R.id.TVusername);
+        tv.setText(username);
     }
 
     @Override
